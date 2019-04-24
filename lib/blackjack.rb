be4fcfb57cb7e096ff986dc701a8851cf31e8c99
Muhammad Musah <mmusah1@gmail.com>
2019-04-24 19:54:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,7 +29,15 @@ return first_round
 end
 
 def hit?
-  # code hit? here
+  prompt_user
+  user_input = get_user_input
+  if user_input == "h"
+    card_total += deal_card
+  elsif user_input == "s"
+    card_total
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
